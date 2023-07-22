bac173ac1b9cf9471836f4f14a4c795f0518072b
RUBEL-CHANDRA-DAS <39419978+RUBEL-CHANDRA-DAS@users.noreply.github.com>
2023-07-22 12:14:32 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// JavaScript code for the slideshow
+let slideIndex = 0;
+
+function showSlides() {
+  const slides = document.getElementsByClassName('slide');
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  setTimeout(showSlides, 2000); // Change the slide every 2 seconds (2000 milliseconds)
+}
+
+// Start the slideshow when the page loads
+window.onload = showSlides;
